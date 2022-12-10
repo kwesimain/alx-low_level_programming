a9359b3274d5831ca7b13d03185eba05e861e056
kwesimain <opokulive@gmail.com>
2022-12-09 16:46:44 -0800

    commit3

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,8 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		printf(num);
+		putchar(num);
 	}
+	putchar('\n);
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,12 +2,12 @@
 #include <ctype.h>
 /**
  * main - prints all the numbers of base 16 in lowercase
- * Retuen: 0
+ * Return: 0
  */
 int main(void)
 {
 	int num;
-	
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
