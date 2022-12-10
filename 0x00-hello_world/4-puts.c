73e3c42338a82ebe89df18091b4a40ec278ae9e2
kwesimain <opokulive@gmail.com>
2022-12-10 03:50:19 -0800

    commit1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Main function of my project
+ * Return: 0
+ */
+int main(void)
+{
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+}
